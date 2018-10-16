15fe1bcb015afbc83a05818398f9cfc4f393a146
torbjorndahlen <torbjorn.dahlen@icloud.com>
2018-10-16 13:48:52 +0200

    added 3scale nodejs client

--- application.js
+++ application.js
@@ -11,8 +11,8 @@ app.use(express.static(__dirname + '/public'));
 
 app.use('/hello', require('./lib/hello.js')());
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080; // must be 8080 to run on OpenShift
+var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'; // must be 0.0.0.0 to run on OpenShift
 app.listen(port, host, function() {
   console.log("App started at: " + new Date() + "on host: " + host + " on port: " + port); 
 });
